3f42d1f2c32f47e3f21d7020aae9cce75826fb55
leutileuti <@>
2021-03-07 22:38:09 +0100

    7.3.2021 9.30 PM

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Plutos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AktienFenster(),
+      home: Homescreen(),
     );
   }
 }
